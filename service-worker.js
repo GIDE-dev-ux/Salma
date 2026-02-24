dba0700a997280414fc2b57f55c0e34228800bdb
GIDE-dev-ux <salmayoussef647@gmail.com>
2026-02-25 00:43:58 +0100

    Add file via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE =  "chatbot-cache-v2";
+
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
